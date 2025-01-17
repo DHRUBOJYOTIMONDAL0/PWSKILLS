408c67cc3d4b2e814dbd30eaba6edd0a07d6149d
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-17 16:46:08 +0530

    Assignment 3

---
+++ PRACTICE_QUESTION/dict.py
@@ -0,0 +1,17 @@
+#Dictionary
+
+d = {"name": "Dhrubojyoti", "email": "dhrubojyotimondal982@gmail.com","contact": 8617510857}
+#name, email and contact are the keys and Dhrubojyoti and number are the values of the keys.
+#print(type(d), d)
+#print(d["contact"])
+d["name"] = "Dhrubo"
+print(len(d))
+#only string and number(int) can be used as a key of the Dictionary
+#s = {"name": "Dhrubojyoti", "email": "dhrubojyotimondal982@gmail.com","contact": 8617510857,"name": "sayan"}
+#s["address"] = "Kolkata" # to create a new key value pair
+# del s["address"] # to delete a key value pair from the dictionary
+# s.clear() # to clear the dictionary and give empty dsictionary
+#print(s) # dictionary used always the last key value pair
+#del s # the hole dictionary will be deleted
+#len(s)
+#print(s)
