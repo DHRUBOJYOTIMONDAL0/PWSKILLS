2653a8899da62929b57b48157135bb24880ea838
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-01-19 14:29:41 +0530

    some

---
+++ PRACTICE_QUESTION/tup_list.py
@@ -0,0 +1,10 @@
+tup = (1,2,3,False,"Dhrubo", True, 9)
+# tup_list = list(tup)
+# print(type(tup_list))
+# slice_tup = tup[2:5]
+# print(slice_tup)
+
+# ind_tup = tup.index(9)
+# print(ind_tup)
+value = 65 in tup
+print(1)
