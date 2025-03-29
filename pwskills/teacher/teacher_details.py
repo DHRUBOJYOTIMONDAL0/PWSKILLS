6370fe864a707e739a9355371cb1e3856fd9ad0c
Dhrubojyoti Mondal <00000dhrubojyoti@gmail.com>
2025-03-29 20:11:53 +0530

    Library , package modules and import statemnet

--- pwskills/teacher/teacher_details.py
+++ pwskills/teacher/teacher_details.py
@@ -1,8 +1,15 @@
 import sys
 from os.path import dirname, join, abspath
-sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+# Add the parent directory to the system path
+parent_dir_path = abspath(join(dirname(__file__), '..'))
+sys.path.insert(0, parent_dir_path)
 
+# Import student_details first before using it
 from student import student_details
+
 def teacher():
     print("This is teacher details")
+
+# Call functions in the correct order
+teacher()
 student_details.student()
